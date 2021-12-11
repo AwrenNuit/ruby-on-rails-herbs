824910302dbc6624bcb6a5a1e6030550b89ee39e
Awren Nuit <awren.nuit@gmail.com>
2021-12-11 16:16:56 -0600

    add Update & Delete functionality

--- app/controllers/herbs_controller.rb
+++ app/controllers/herbs_controller.rb
@@ -26,6 +26,27 @@ class HerbsController < ApplicationController
     end
   end
 
+  def edit
+    @herb = Herb.find(params[:id])
+  end
+
+  def update
+    @herb = Herb.find(params[:id])
+
+    if(@herb.update(herb_params))
+      redirect_to @herb
+    else
+      render 'edit'
+    end
+  end
+
+  def destroy
+    @herb = Herb.find(params[:id])
+    @herb.destroy
+
+    redirect_to herbs_path
+  end
+
   # defines params needed for create method
   private def herb_params
     params.require(:herb).permit(:name, :description)
